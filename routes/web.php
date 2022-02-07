8f3d3cd9bcbfd97b0ede2fb4d16e41db830e98ce
AxelDavin <3103120043@student.smktelkom-pwt.sch.id>
2022-02-07 12:54:33 +0700

    modul 2

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/home', function () {
-    return view ('home');
+    return view ('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "nama" => "Axel Davin Lazar Panenggak",
         "email" => "axeldavin2005@gmail.com",
         "gambar" => "fotoaxel.jpg"
@@ -30,5 +33,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view ('gallery');
+    return view ('gallery', [
+        "title" => "Gallery"
+    ]);
 });
