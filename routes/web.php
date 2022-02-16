40ef190ab7b8b7309fbc23d5f6a32a5ae277875b
AxelDavin <3103120043@student.smktelkom-pwt.sch.id>
2022-02-16 17:46:03 +0700

    modul 4

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/home', function () {
     return view ('home', [
         "title" => "Home"
     ]);
